b3b98180f6cecc8525ff136bff391fab23588624
LAPTOP-4M8M5MON\Martin_Aldair <martin.a.rmz.ramos@gmail.com>
2023-01-06 12:37:09 -0600

    @Dev-00-1 - Se agrego la entidad de persistencia Person - Se añadio la libreria java persistence api para utilizar las anotaciones (Por ejemplo @Entity,@Column) necesarias para la entidad de persistencia Person

---
+++ src/main/java/mx/examen/entity/Person.java
@@ -0,0 +1,107 @@
+package mx.examen.entity;
+
+import java.io.Serializable;
+import javax.persistence.*;
+
+@Entity
+@Table(name = "person")
+public class Person implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "nombre")
+    private String nombre;
+
+    @Column(name = "apellidos")
+    private String apellidos;
+
+    @Column(name = "rfc")
+    private String rfc;
+
+    @Column(name = "curp")
+    private String curp;
+
+    @Column(name = "edad")
+    private String edad;
+
+    @Column(name = "sexo")
+    private String sexo;
+
+    @Column(name = "nacionalidad")
+    private String nacionalidad;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public String getCurp() {
+        return curp;
+    }
+
+    public void setCurp(String curp) {
+        this.curp = curp;
+    }
+
+    public String getEdad() {
+        return edad;
+    }
+
+    public void setEdad(String edad) {
+        this.edad = edad;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getNacionalidad() {
+        return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" + "id=" + id + ", nombre=" + nombre + ", apellidos=" + apellidos + ", rfc=" + rfc + ", curp=" + curp + ", edad=" + edad + ", sexo=" + sexo + ", nacionalidad=" + nacionalidad + '}';
+    }
+
+}
