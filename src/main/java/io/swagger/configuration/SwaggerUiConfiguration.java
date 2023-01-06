38d488e8511b7f821d8f4b030fe1b80f4a057900
LAPTOP-4M8M5MON\Martin_Aldair <martin.a.rmz.ramos@gmail.com>
2023-01-06 14:37:31 -0600

    @Dev-00-5 - Se personalizo el contenido de la interfaz de usuario de OpenApi-UI 3.0 (Swagger). Referencia: https://springdoc.org/#source-code-of-the-demo-applications

---
+++ src/main/java/io/swagger/configuration/SwaggerUiConfiguration.java
@@ -0,0 +1,46 @@
+package io.swagger.configuration;
+
+import io.swagger.v3.oas.models.*;
+import io.swagger.v3.oas.models.info.*;
+import io.swagger.v3.oas.models.servers.Server;
+import java.util.Arrays;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@javax.annotation.Generated(value = "io.swagger.codegen.v3.generators.java.SpringCodegen", date = "2023-01-06T19:40:43.392Z[GMT]")
+@Configuration
+public class SwaggerUiConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        Server localServer = new Server();
+        localServer.setDescription("local");
+        localServer.setUrl("http://localhost:8080");
+        OpenAPI openAPI = new OpenAPI();
+        openAPI.info(info());
+        openAPI.setExternalDocs(externalDocumentation());
+        openAPI.setServers(Arrays.asList(localServer));
+        return openAPI;
+    }
+
+    private Info info() {
+        return new Info()
+                .title("Examen - OpenAPI 3.0")
+                .description("Este es una Api basado en la especificación OpenAPI 3.0. "
+                        + "Se Puede obtener más información sobre la API en [https://examen.mx](https://examen.mx).")
+                .termsOfService("http://examen.mx/terms/")
+                .version("1.0.01")
+                .contact(new Contact()
+                        .email("examen@mail.mx"))
+                .license(new License()
+                        .name("Apache 2.0")
+                        .url("http://www.apache.org/licenses/LICENSE-2.0.html"));
+    }
+
+    private ExternalDocumentation externalDocumentation() {
+        return new ExternalDocumentation()
+                .description("Más información sobre el examen")
+                .url("http://examen.mx");
+    }
+
+}
