8a26bc37f9d6049888870d9aa4b52a3dccdaffe0
LAPTOP-4M8M5MON\Martin_Aldair <martin.a.rmz.ramos@gmail.com>
2023-01-06 18:15:01 -0600

    @Dev-01-1 - Se agrega el ejercicio 5.1 en el archivo ExamenApplicationTests

--- src/test/java/mx/examen/ExamenApplicationTests.java
+++ src/test/java/mx/examen/ExamenApplicationTests.java
@@ -1,13 +1,37 @@
 package mx.examen;
 
 import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class ExamenApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
+
+    /*
+    5.	Manejo de Errores
+    5.1	Dado la siguiente clase agrega el manejo de errores correspondientes y justifica él porque
+     */
+    @Test
+    void ejercicio5_1() {
+        System.out.println("Intentamos ejecutar el bloque de instrucciones:");
+        System.out.println("Instrucción 1.");
+        /* 
+        En el presente ejercicio se utiliza el bloque try-catch, porque sirve para
+        saber el tipo de excepción que esta en el metodo parseInt() de la clase Integer.
+        Al momento ejecutarse el metodo parseInt() y al no encontrar caracteres de
+        tipo numerico y solamente letras, se produce una excepción de tipo 
+        java.lang.NunmberFormatException
+         */
+        try {
+            int n = Integer.parseInt("M"); //error forzado en tiempo de ejecución.
+        } catch (Exception ex) {
+            log.error("Tipo de error: " + ex);
+        }
+        System.out.println("Instrucción 2.");
+        System.out.println("Instrucción 3, etc.");
+    }
 
 }
